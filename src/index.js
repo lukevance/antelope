304e24f7f1d93cc2070cfab9c5ebbda0f6223eee
Luke Vance <lukevance90@gmail.com>
2017-09-07 22:54:17 -0600

    added simple token generator

--- src/index.js
+++ src/index.js
@@ -2,15 +2,21 @@ const app = require('express')();
 const bodyParser = require('body-parser');
 const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
 const schema = require('./schema');
+const {authenticate} = require('./authentication');
 
 const connectMongo = require('./mongo-connector');
 
 const start = async () => {
 	const mongo = await connectMongo();
-	app.use('/graphql', bodyParser.json(), graphqlExpress({ 
-		context: { mongo },
-		schema 
-	}));
+
+	const buildOptions = async (req, res) => {
+		const user = await authenticate(req, mongo.Users);	
+		return {
+			context: {mongo, user},
+			schema
+		};
+	};
+	app.use('/graphql', bodyParser.json(), graphqlExpress(buildOptions));
 	app.use('/graphiql', graphiqlExpress({
 	    endpointURL: '/graphql'
 	}));
